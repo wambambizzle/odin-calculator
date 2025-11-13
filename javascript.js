59b3ec69be4360d5658a2a67412732dd3555aefa
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-13 12:40:12 -0500

    Make clac numbers show in display - add event listener to calc-numbers - write inputs to clac display - store value in firstNumber var

--- javascript.js
+++ javascript.js
@@ -1,38 +1,63 @@
-// Holder variables
+// Holder Vars
 
-let firstNumber = ""
-let secondNumber = ""
-let operator = ""
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+// DOM Vars
+
+const display = document.querySelector(".calculator-display");
+const calculatorNumbers = document.querySelector(".calculator-numbers");
 
 // Math Functions
 
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
 
 function operate(operator, num1, num2) {
   switch (operator) {
     case "+":
-      return add(num1, num2)
+      return add(num1, num2);
     case "-":
-      return subtract(num1, num2)
+      return subtract(num1, num2);
     case "*":
-      return multiply(num1, num2)
+      return multiply(num1, num2);
     case "/":
-      return divide(num1, num2)
+      return divide(num1, num2);
     default:
       break;
   }
 }
+
+// Event Listeners
+
+function setCalculatorNumbersListeners() {
+  calculatorNumbers.childNodes.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (display.textContent === "0") {
+        display.textContent = button.textContent;
+      } else {
+        display.textContent += button.textContent;
+      }
+
+      firstNumber = display.textContent;
+    });
+  });
+}
+
+setCalculatorNumbersListeners();
+
+// Calculator Functions
