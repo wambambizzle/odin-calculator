bfbff5beb760aa6546007e058bb071a76d5e5fd7
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-13 11:37:28 -0500

    Add the operate function - takes an operand and two numbers - switches on the operand - then used one of our math funcs

--- javascript.js
+++ javascript.js
@@ -21,3 +21,18 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2)
+    case "-":
+      return subtract(num1, num2)
+    case "*":
+      return multiply(num1, num2)
+    case "/":
+      return divide(num1, num2)
+    default:
+      break;
+  }
+}
